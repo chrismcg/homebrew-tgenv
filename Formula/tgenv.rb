7c4825fb6e43cdb9b0de158083719b8875219d1f
Chris McGrath <chris@chrismcg.com>
2021-02-11 11:05:41 +0000

    Point to faknorr/tgenv
    This is to allow installing very old versions that don't have sha's

--- Formula/tgenv.rb
+++ Formula/tgenv.rb
@@ -1,9 +1,9 @@
 class Tgenv < Formula
   desc "Terragrunt version manager inspired by tfenv"
-  homepage "https://github.com/sigsegv13/tgenv"
+  homepage "https://github.com/faknorr/tgenv"
   url "https://github.com/sigsegv13/tgenv/archive/v0.0.8.tar.gz"
-  sha256 "13790b71c0f7fbdc48ca22a794481ac443faabd9e9facba4639b0f6cc19d674f"
-  head "https://github.com/sigsegv13/tgenv.git"
+  sha256 "e987fa5adf0be150c6b28aa92d5b9edfc3c139fbe50dfeec5d1dbc1d066506d4"
+  head "https://github.com/faknorr/tgenv.git"
 
   bottle :unneeded
 
